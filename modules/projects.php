da3ad79f6903d91196af0e8c133f4581d0afb8e3
romanproudly <roman@proudly.ru>
2021-04-24 16:33:29 +0300

    Update projects.php

--- modules/projects.php
+++ modules/projects.php
@@ -12,12 +12,14 @@ class projects extends krn_abstract {
 		if (!$_LEVEL[1]) $_LEVEL[1] = 'application';
 		
 		if ($this->code = $_LEVEL[1]) {
-			$this->project = $this->db->getRow('SELECT Id, Title, Code, SeoTitle, SeoKeywords, SeoDescription FROM projects WHERE Code = ?s', $this->code);
+			$this->project = $this->db->getRow('SELECT Id, Title, Code, SeoTitle, SeoKeywords, SeoDescription FROM projects WHERE Code = ?s AND Lang = ?s', $this->code, $this->lang->GetLang());
 			$this->pageTitle = $this->project['Title'];
+			/*
 			$this->breadCrumbs = GetBreadCrumbs(array(
 				'Главная' => '/',
 				$this->page['Title'] => $this->page['Code'] . '/'),
 				$this->pageTitle);
+				*/
 
 		}	
 	}	
@@ -38,7 +40,7 @@ class projects extends krn_abstract {
 	}
 
 	function GetProject() {
-		$projects = $this->db->getCol('SELECT Code FROM projects ORDER BY IF(`Order`, -100/`Order`, 0)');
+		$projects = $this->db->getCol('SELECT Code FROM projects WHERE Lang = ?s ORDER BY IF(`Order`, -100/`Order`, 0)', $this->lang->GetLang());
 
 		return strtr($this->lang->LoadTemplate($this->code), array(
 			'<%LINK1%>'				=> $projects[0] . '/',
