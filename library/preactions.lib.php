080158603df0da094ab302e726c966bb95d59dba
romanproudly <roman@proudly.ru>
2021-04-24 15:32:32 +0300

    Update preactions.lib.php

--- library/preactions.lib.php
+++ library/preactions.lib.php
@@ -27,6 +27,7 @@
 		 	$this->db = $Params['Db']['Link'];
 
 		 	if ($_GET['p_code'] == 'set-lang') {
+		 		krnLoadLib('define');
 		 		setcookie('lang_selected_by_user', '1', time() + WEEK_IN_SEC*24, '/', $_SERVER['HTTP_HOST'], true);
 		 		$_COOKIE['lang_selected_by_user'] = 1;
 				__Redirect('/');
